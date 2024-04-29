e3c246d6adb0b594d85927cad05d57753a34a621
deadshumz <shumzz@bk.ru>
2024-04-29 21:58:31 +0300

    User creation endpoint

--- src/main/java/com/example/financeapp/controller/UserController.java
+++ src/main/java/com/example/financeapp/controller/UserController.java
@@ -4,12 +4,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.financeapp.dto.UserRegister;
+import com.example.financeapp.exceptions.UserAlreadyExistException;
+import com.example.financeapp.repository.entity.User;
 import com.example.financeapp.service.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/api/user")
 public class UserController {
     
     @Autowired
     private UserService userService;
+
+    @PostMapping(value = "/create", consumes = {"application/json"})
+    public String createUser(@RequestBody UserRegister userRegister) {
+        try {
+            User user = userService.registerUser(userRegister);
+            return String.format("Success user #%s registered", user.getId());
+        } catch (UserAlreadyExistException e) {
+            return "User already exist";
+        }
+    }
 }
--- src/main/java/com/example/financeapp/repository/TransactionRepository.java
+++ src/main/java/com/example/financeapp/repository/TransactionRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.financeapp.repository.entity.Transaction;
 
-public interface TransactionRepository extends JpaRepository<Transaction, Long> {}
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    
+}
