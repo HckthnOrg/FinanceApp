e8eee77c05ff54485fe4c1fb846856bfe657a2b3
Denis Karpov <karpovdenis.al@gmail.com>
2024-05-06 12:56:15 +0300

    Add create method

--- src/main/java/com/example/financeapp/service/UserService.java
+++ src/main/java/com/example/financeapp/service/UserService.java
@@ -1,10 +1,9 @@
 package com.example.financeapp.service;
 
-import com.example.financeapp.dto.UserRegisterDto;
-import com.example.financeapp.exceptions.UserAlreadyExistException;
+import com.example.financeapp.exceptions.EmailAlreadyTakenException;
+import com.example.financeapp.exceptions.UserAlreadyExistsException;
 import com.example.financeapp.repository.UserRepository;
 import com.example.financeapp.repository.entity.User;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -21,19 +20,15 @@ public class UserService {
         return userRepository.save(user);
     }
 
-    @Transactional
-    public User registerUser(UserRegisterDto userDto) throws UserAlreadyExistException {
-        if (userRepository.findByLogin(userDto.getLogin()) != null) {
-            throw new UserAlreadyExistException();
+    public User create(User user) {
+        if (userRepository.existsByUsername(user.getUsername())) {
+            throw new UserAlreadyExistsException();
         }
 
-        User newUser = new User();
-        newUser.setLogin(userDto.getLogin());
-        newUser.setPassword(passwordEncoder.encode(userDto.getPassword()));
-        newUser.setFirstName(userDto.getFirstName());
-        newUser.setLastName(userDto.getLastName());
-        newUser.setAccount(accountService.createAccount(newUser));
+        if (userRepository.existsByEmail(user.getEmail())) {
+            throw new EmailAlreadyTakenException();
+        }
 
-        return userRepository.save(newUser);
+        return save(user);
     }
 }
