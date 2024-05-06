40f0ba7f06b4de5187700b14b5e58019206379f0
Denis Karpov <karpovdenis.al@gmail.com>
2024-05-06 21:50:16 +0300

    Remove unnecessary variables

--- src/main/java/com/example/financeapp/service/UserService.java
+++ src/main/java/com/example/financeapp/service/UserService.java
@@ -8,16 +8,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
-
     private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
-    private final AccountService accountService;
 
     public User save(User user) {
         return userRepository.save(user);
