430157dfeea2d2bbea7f785633f7c47b708c6fd7
Denis Karpov <karpovdenis.al@gmail.com>
2024-05-06 12:51:01 +0300

    Refactor to use Optional and change method name

--- src/main/java/com/example/financeapp/repository/UserRepository.java
+++ src/main/java/com/example/financeapp/repository/UserRepository.java
@@ -3,6 +3,8 @@ package com.example.financeapp.repository;
 import com.example.financeapp.repository.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByLogin(String login);
+    Optional<User> findByUsername(String username);
 }
