29aeb84ad73197a47aee840dc359293ce5d2905e
Denis Karpov <karpovdenis.al@gmail.com>
2024-05-06 12:42:31 +0300

    Change the extended class to a more specific one

--- src/main/java/com/example/financeapp/exceptions/UserAlreadyExistsException.java
+++ src/main/java/com/example/financeapp/exceptions/UserAlreadyExistsException.java
@@ -1,6 +1,6 @@
 package com.example.financeapp.exceptions;
 
-public class UserAlreadyExistsException extends Exception {
+public class UserAlreadyExistsException extends RuntimeException {
     public UserAlreadyExistsException() {
         super("User with this username already exist.");
     }
