0a35dada32de9742f1017d653216baea1ccff005
Denis Karpov <karpovdenis.al@gmail.com>
2024-05-06 12:33:49 +0300

    Change login to username

--- src/main/java/com/example/financeapp/exceptions/UserAlreadyExistException.java
+++ src/main/java/com/example/financeapp/exceptions/UserAlreadyExistException.java
@@ -2,6 +2,6 @@ package com.example.financeapp.exceptions;
 
 public class UserAlreadyExistException extends Exception {
     public UserAlreadyExistException() {
-        super("User with this login already exist.");
+        super("User with this username already exist.");
     }
 }
