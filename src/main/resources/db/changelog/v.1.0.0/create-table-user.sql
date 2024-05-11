29f2efca55d75c8ba43f362a1b0cc6e5600ea486
Denis Karpov <karpovdenis.al@gmail.com>
2024-05-11 22:05:02 +0300

    Add users table creation script

---
+++ src/main/resources/db/changelog/v.1.0.0/create-table-user.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS users
+(
+    id          SERIAL PRIMARY KEY,
+    username    VARCHAR(255) UNIQUE NOT NULL,
+    password    VARCHAR(255)        NOT NULL,
+    email       VARCHAR(255) UNIQUE NOT NULL,
+    first_name  VARCHAR(255)        NOT NULL,
+    second_name VARCHAR(255)        NOT NULL,
+    role        VARCHAR(50)         NOT NULL,
+    created_at  TIMESTAMP           NOT NULL DEFAULT CURRENT_TIMESTAMP,
+);
