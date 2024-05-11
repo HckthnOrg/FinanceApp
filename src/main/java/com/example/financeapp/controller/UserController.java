317395384036869d7bbc191a0fd0613a934afd94
Denis Karpov <karpovdenis.al@gmail.com>
2024-05-11 20:42:30 +0300

    Remove old imports

--- src/main/java/com/example/financeapp/controller/UserController.java
+++ src/main/java/com/example/financeapp/controller/UserController.java
@@ -1,15 +1,6 @@
 package com.example.financeapp.controller;
 
-import com.example.financeapp.dto.UserRegisterDto;
-import com.example.financeapp.dto.UserResponseDto;
-import com.example.financeapp.exception.UserAlreadyExistsException;
-import com.example.financeapp.repository.entity.User;
-import com.example.financeapp.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
