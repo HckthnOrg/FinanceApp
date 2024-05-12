7bcd1d0099a416b2eed46b585b7351ba4f7c924e
Denis Karpov <karpovdenis.al@gmail.com>
2024-05-12 15:07:51 +0300

    Remove redundant comma

--- src/main/resources/db/changelog/v.1.0.1/create-table-user.sql
+++ src/main/resources/db/changelog/v.1.0.1/create-table-user.sql
@@ -7,5 +7,5 @@ CREATE TABLE IF NOT EXISTS users
     first_name  VARCHAR(255)        NOT NULL,
     second_name VARCHAR(255)        NOT NULL,
     role        VARCHAR(50)         NOT NULL,
-    created_at  TIMESTAMP           NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    created_at  TIMESTAMP           NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
