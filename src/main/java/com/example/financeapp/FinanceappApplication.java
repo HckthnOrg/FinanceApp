106a7fd85c68f258a3d2d8e6b7cb20fd48843274
Denis Karpov <karpovdenis.al@gmail.com>
2024-05-12 21:03:12 +0300

    Remove redundant imports

--- src/main/java/com/example/financeapp/FinanceappApplication.java
+++ src/main/java/com/example/financeapp/FinanceappApplication.java
@@ -1,19 +1,14 @@
 package com.example.financeapp;
 
-import com.example.financeapp.config.SecurityConfig;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import(SecurityConfig.class)
 public class FinanceappApplication {
-
     public static void main(String[] args) {
         Dotenv dotenv = Dotenv.load();
         dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
         SpringApplication.run(FinanceappApplication.class, args);
     }
-
 }
