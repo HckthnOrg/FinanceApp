104324a2c0a0f95864099a6bc8c08846436b0d8f
Denis Karpov <karpovdenis.al@gmail.com>
2024-05-14 21:11:19 +0300

    Add repository annotation

--- src/main/java/com/example/financeapp/repository/TransactionRepository.java
+++ src/main/java/com/example/financeapp/repository/TransactionRepository.java
@@ -2,6 +2,8 @@ package com.example.financeapp.repository;
 
 import com.example.financeapp.domain.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 }
