53a85f2e62cc7326a56fb7cf16b9ad9719956bf2
Denis Karpov <karpovdenis.al@gmail.com>
2024-05-16 14:03:23 +0300

    Rename total spend to balance

--- src/main/resources/db/changelog/v.1.1.2/create-table-accounts.sql
+++ src/main/resources/db/changelog/v.1.1.2/create-table-accounts.sql
@@ -2,7 +2,7 @@ CREATE TABLE accounts
 (
     id                 SERIAL PRIMARY KEY,
     user_id            BIGINT NOT NULL,
-    total_spent        BIGINT,
+    balance            BIGINT,
     transaction_amount BIGINT,
     created_at         TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users (id)
