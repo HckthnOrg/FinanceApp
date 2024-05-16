a7547bd069bf3869620d2092d59eebcd782e4375
Denis Karpov <karpovdenis.al@gmail.com>
2024-05-16 14:04:02 +0300

    Update changelog

--- src/main/resources/db/changelog/v.1.1.2/create-table-user.sql
+++ src/main/resources/db/changelog/v.1.1.2/create-table-user.sql
@@ -5,7 +5,7 @@ CREATE TABLE users
     password    VARCHAR(255)        NOT NULL,
     email       VARCHAR(255) UNIQUE NOT NULL,
     first_name  VARCHAR(255),
-    last_name VARCHAR(255),
+    last_name   VARCHAR(255),
     role        VARCHAR(50)         NOT NULL,
     created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
